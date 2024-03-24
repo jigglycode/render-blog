52d60c5a7b30f987376165350cce0f38e7d82e00
olivia <yoon.eunsil@gmail.com>
2024-03-24 14:47:24 -0700

    Add additional error types to errorHandler Create tokenExtractor and userExtractor middleware to handle token auth for blogsRouter Create routes for login and user in app Refactor post and delete blog methods to only work for authenticated users

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
@@ -15,26 +19,35 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body
-  if (!body.title || !body.url) {
-    return response.status(400).end()
-  }
+  const user = request.user
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user.id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  const user = request.user
+
+  if (blog.user.toString() === user.id) {
+    await Blog.findByIdAndDelete(blog._id)
+    response.status(204).end()
+  } else {
+    response.status(400).json({ error: 'only user who added can delete blog' })
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
