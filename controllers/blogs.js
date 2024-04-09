4fa29a4d3d196592624a3818e0a423e81864fb7a
olivia <yoon.eunsil@gmail.com>
2024-04-08 17:07:00 -0700

    Create comments for blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,65 +1,78 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const { userExtractor } = require('../utils/middleware')
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const Comment = require('../models/comment');
+const { userExtractor } = require('../utils/middleware');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({})
+  const blogs = await Blog.find({})
     .sort({ likes: -1 })
-    .populate('user', { username: 1, name: 1 })
+    .populate('user', { username: 1, name: 1 });
 
-  response.json(blogs)
-})
+  response.json(blogs);
+});
 
 blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+  const blog = await Blog.findById(request.params.id);
   if (blog) {
-    response.json(blog)
+    response.json(blog);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
 blogsRouter.post('/', userExtractor, async (request, response) => {
-  const body = request.body
-  const user = request.user
+  const body = request.body;
+  const user = request.user;
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: user.id
-  })
+    user: user.id,
+  });
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
 
-  response.status(201).json(savedBlog)
-})
+  response.status(201).json(savedBlog);
+});
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const blog = awaitBlog.findById(request.params.id);
+  const comment = new Comment({
+    title: body.title,
+    blog: blog.id,
+  });
+
+  const savedComment = await comment.save();
+  blog.comments = blog.comments.concat(savedComment._id);
+  await blog.save();
+
+  response.status(201).json(savedComment);
+});
 
 blogsRouter.delete('/:id', userExtractor, async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
-  const user = request.user
+  const blog = await Blog.findById(request.params.id);
+  const user = request.user;
 
   if (blog.user.toString() === user.id) {
-    await Blog.findByIdAndDelete(blog._id)
-    response.status(204).end()
+    await Blog.findByIdAndDelete(blog._id);
+    response.status(204).end();
   } else {
-    response.status(400).json({ error: 'only user who added can delete blog' })
+    response.status(400).json({ error: 'only user who added can delete blog' });
   }
-})
+});
 
 blogsRouter.put('/:id', async (request, response) => {
-  const updatedBlog = await Blog
-    .findByIdAndUpdate(
-      request.params.id,
-      { likes: request.body.likes },
-      { new: true }
-    )
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { likes: request.body.likes },
+    { new: true }
+  );
 
-  response.json(updatedBlog)
-})
+  response.json(updatedBlog);
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
